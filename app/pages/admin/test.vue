c0f7b8685f92036efc57b59e57a54c4dab2660be
jatnipit <jatnipt.k@ku.th>
2025-09-16 09:13:04 +0700

    apply server utils in line send-message

---
+++ app/pages/admin/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ responseMessage }}</div>
+  <button @click="testApi">Test API</button>
+</template>
+
+<script setup>
+const responseMessage = ref("");
+
+async function testApi() {
+  const response = await $fetch("/api/line/test", {
+    method: "POST",
+    body: {
+      userId: "U0b6ef66e72182ad3a91d83ba40270ff6",
+      message: "test",
+    },
+  });
+  responseMessage.value = response.message;
+}
+</script>
+
+<style></style>
