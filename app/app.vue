0fa6285701f86d48ea42ecf64f125343d50d7fe9
jatnipit <jatnipt.k@ku.th>
2025-09-18 13:20:39 +0700

    apply select2

--- app/app.vue
+++ app/app.vue
@@ -19,6 +19,14 @@ useHead({
       rel: "stylesheet",
       href: "https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css",
     },
+    {
+      rel: "stylesheet",
+      href: "https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://cdn.jsdelivr.net/npm/select2-bootstrap4-theme@1.4.6/dist/select2-bootstrap4.min.css",
+    },
   ],
   script: [
     {
@@ -27,6 +35,10 @@ useHead({
     },
     {
       src: "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js",
+      body: true,
+    },
+    {
+      src: "https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js",
       body: true,
     },
     {
